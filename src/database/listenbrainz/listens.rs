fc5b0e99fe24e496264f898f52a89edf62a4e9ae
RustyNova <rusty.nova.jsb@gmail.com>
2025-02-10 14:15:51 +0000

    work

--- src/database/listenbrainz/listens.rs
+++ src/database/listenbrainz/listens.rs
@@ -11,61 +11,61 @@ use tuillez::pg_inc;
 use crate::api::clients::ALISTRAL_CLIENT;
 use crate::utils::env::in_offline_mode;
 
-#[derive(Builder)]
-pub struct ListenFetchQuery {
-    #[builder(Default=!)]
-    user: String,
+// #[derive(Builder)]
+// pub struct ListenFetchQuery {
+//     #[builder(Default=!)]
+//     user: String,
 
-    fetch_recordings_redirects: bool,
+//     fetch_recordings_redirects: bool,
 
-    returns: ListenFetchQueryReturn,
-}
+//     returns: ListenFetchQueryReturn,
+// }
 
-impl ListenFetchQuery {
-    pub async fn fetch(
-        self,
-        conn: &mut sqlx::SqliteConnection,
-    ) -> Result<ListenCollection, crate::Error> {
-        // Fetch the latest listens
-        // ... If it's not in offline mode
-        if !in_offline_mode() {
-            fetch_latest_listens_of_user(&ALISTRAL_CLIENT, conn, &self.user).await?;
-        }
+// impl ListenFetchQuery {
+//     pub async fn fetch(
+//         self,
+//         conn: &mut sqlx::SqliteConnection,
+//     ) -> Result<ListenCollection, crate::Error> {
+//         // Fetch the latest listens
+//         // ... If it's not in offline mode
+//         if !in_offline_mode() {
+//             fetch_latest_listens_of_user(&ALISTRAL_CLIENT, conn, &self.user).await?;
+//         }
 
-        if self.fetch_recordings_redirects {
-            Self::fetch_recordings_redirects(conn, &self.user).await?;
-        }
+//         if self.fetch_recordings_redirects {
+//             Self::fetch_recordings_redirects(conn, &self.user).await?;
+//         }
 
-        match self.returns {
-            ListenFetchQueryReturn::Mapped => Ok(ListenCollection::new(
-                Listen::get_mapped_listen_of_user(conn, &self.user).await?,
-            )),
-            ListenFetchQueryReturn::Unmapped => Ok(ListenCollection::new(
-                Listen::get_unmapped_listen_of_user(conn, &self.user).await?,
-            )),
-            ListenFetchQueryReturn::None => Ok(ListenCollection::default()),
-        }
-    }
+//         match self.returns {
+//             ListenFetchQueryReturn::Mapped => Ok(ListenCollection::new(
+//                 Listen::get_mapped_listen_of_user(conn, &self.user).await?,
+//             )),
+//             ListenFetchQueryReturn::Unmapped => Ok(ListenCollection::new(
+//                 Listen::get_unmapped_listen_of_user(conn, &self.user).await?,
+//             )),
+//             ListenFetchQueryReturn::None => Ok(ListenCollection::default()),
+//         }
+//     }
 
-    #[instrument(fields(indicatif.pb_show = tracing::field::Empty))]
-    async fn fetch_recordings_redirects(
-        conn: &mut SqliteConnection,
-        user: &str,
-    ) -> Result<(), crate::Error> {
-        let unfetched = Listen::get_unfetched_recordings_of_user(conn, user).await?;
-        pg_counted!(unfetched.len(), "Fetching listen MBIDs");
+//     #[instrument(fields(indicatif.pb_show = tracing::field::Empty))]
+//     async fn fetch_recordings_redirects(
+//         conn: &mut SqliteConnection,
+//         user: &str,
+//     ) -> Result<(), crate::Error> {
+//         let unfetched = Listen::get_unfetched_recordings_of_user(conn, user).await?;
+//         pg_counted!(unfetched.len(), "Fetching listen MBIDs");
 
-        for id in unfetched {
-            Recording::get_or_fetch(conn, &ALISTRAL_CLIENT.musicbrainz_db, &id).await?;
-            pg_inc!();
-        }
+//         for id in unfetched {
+//             Recording::get_or_fetch(conn, &ALISTRAL_CLIENT.musicbrainz_db, &id).await?;
+//             pg_inc!();
+//         }
 
-        Ok(())
-    }
-}
+//         Ok(())
+//     }
+// }
 
-pub enum ListenFetchQueryReturn {
-    Mapped,
-    Unmapped,
-    None,
-}
+// pub enum ListenFetchQueryReturn {
+//     Mapped,
+//     Unmapped,
+//     None,
+// }
--- src/testing/fixtures/user_listens.rs
+++ src/testing/fixtures/user_listens.rs
@@ -1,11 +1,11 @@
+use alistral_core::database::fetching::listens::ListenQuery;
 use alistral_core::datastructures::entity_with_listens::recording::collection::RecordingWithListensCollection;
 use alistral_core::datastructures::listen_collection::ListenCollection;
 
 use crate::api::clients::ALISTRAL_CLIENT;
-use crate::database::listenbrainz::listens::ListenFetchQuery;
-use crate::database::listenbrainz::listens::ListenFetchQueryReturn;
 
 pub async fn get_test_user_listens() -> ListenCollection {
+    ListenQuery {}
     ListenFetchQuery::builder()
         .fetch_recordings_redirects(true)
         .returns(ListenFetchQueryReturn::Mapped)
