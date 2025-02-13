03b4994b2935007ac06de055fc77156fd4ae7a4a
RustyNova <rusty.nova.jsb@gmail.com>
2025-02-13 18:11:40 +0000

    feat: rework of the variable system

--- sonartra/src/error.rs
+++ sonartra/src/error.rs
@@ -4,4 +4,13 @@ use thiserror::Error;
 pub enum Error {
     #[error("Te")]
     ReadVariableError(serde_json::Error),
+
+    #[error("A variable was missing during the radio compilation. Step id: {0}, variable name: {1}")]
+    MissingVariableError(String, String),
+
+    #[error("Variable {0} has the wrong type. Expected `{1}`, got `{2}`")]
+    WrongVariableTypeError(String, String, String),
+
+    #[error("Couldn't compile the radio due to incorect variables: {0}")]
+    VariableReadError(serde_json::Error),
 }
