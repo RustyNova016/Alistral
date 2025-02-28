8e92ab4388f13e48a14915652b4b83994398c605
RustyNova <rusty.nova.jsb@gmail.com>
2025-02-28 09:08:56 +0000

    work

--- sonartra/src/radio_item.rs
+++ sonartra/src/radio_item.rs
@@ -1,7 +1,16 @@
+use core::ops::Deref;
+
 use alistral_core::datastructures::entity_with_listens::recording::RecordingWithListens;
 
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub struct RadioItem {
     recording: RecordingWithListens,
     score: i64,
+}
+
+impl Deref for RadioItem {
+    type Target = RecordingWithListens;
+    fn deref(&self) -> &Self::Target {
+        &self.recording
+    }
 }
