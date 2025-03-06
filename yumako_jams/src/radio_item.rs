6579ccff308478b5461871eabadcfac9631da6c8
RustyNova <rusty.nova.jsb@gmail.com>
2025-03-06 08:48:20 +0000

    feat: add listenrate sorter

--- yumako_jams/src/radio_item.rs
+++ yumako_jams/src/radio_item.rs
@@ -1,11 +1,26 @@
 use core::ops::Deref;
 
 use alistral_core::datastructures::entity_with_listens::recording::RecordingWithListens;
+use rust_decimal::Decimal;
+
+use crate::modules::scores::ScoreMerging;
 
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub struct RadioItem {
     recording: RecordingWithListens,
-    pub score: i64,
+    pub score: Decimal,
+}
+
+impl RadioItem {
+    pub fn set_score(&mut self, score: Decimal, merge: ScoreMerging) {
+        match merge {
+            ScoreMerging::Replace => self.score = score,
+            ScoreMerging::Add => self.score += score,
+            ScoreMerging::Sub => self.score -= score,
+            ScoreMerging::Multiply => self.score *= score,
+            ScoreMerging::Divide => self.score /= score,
+        }
+    }
 }
 
 impl Deref for RadioItem {
@@ -19,7 +34,7 @@ impl From<RecordingWithListens> for RadioItem {
     fn from(value: RecordingWithListens) -> Self {
         Self {
             recording: value,
-            score: 0,
+            score: Decimal::ZERO,
         }
     }
 }
