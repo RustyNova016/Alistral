21d2ff22f0a4d429f84b4713f5bb3a7a0a5e9486
RustyNova <rusty.nova.jsb@gmail.com>
2025-03-10 09:50:39 +0000

    refactor: move aliases to other modules

--- yumako_jams/src/aliases.rs
+++
@@ -1,9 +0,0 @@
-use futures::stream::BoxStream;
-
-use crate::radio_item::RadioItem;
-
-pub type LayerResult<'a> = Result<RadioStream<'a>, crate::Error>;
-
-pub type RadioResult = Result<RadioItem, crate::Error>;
-
-pub type RadioStream<'a> = BoxStream<'a, RadioResult>;
