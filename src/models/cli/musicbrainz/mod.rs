b1858d7f4925d8b5bbdb56842a94263943a28a3d
RustyNova <rusty.nova.jsb@gmail.com>
2025-04-24 08:22:13 +0000

    refactor: moved MusicbrainzClippyCommand to tool file

--- src/models/cli/musicbrainz/mod.rs
+++ src/models/cli/musicbrainz/mod.rs
@@ -1,9 +1,7 @@
 use clap::Parser;
 use clap::Subcommand;
 
-use crate::tools::musicbrainz::clippy::mb_clippy;
-use crate::utils::cli::read_mbid_from_input;
-use crate::utils::whitelist_blacklist::WhitelistBlacklist;
+use crate::tools::musicbrainz::clippy::MusicbrainzClippyCommand;
 
 #[derive(Parser, Debug, Clone)]
 #[command(version, about, long_about = None)]
@@ -20,56 +18,13 @@ impl MusicbrainzCommand {
 
 #[derive(Subcommand, Debug, Clone)]
 pub enum MusicbrainzSubcommands {
-    /// Search for potential mistakes, missing data and style issues. This allows to quickly pin down errors that can be corrected
-    ///
-    /// ⚠️ All tips are suggestions. Take them with a grain of salt. If you are unsure, it's preferable to skip.
-    Clippy {
-        /// The MBID of a recording to start from
-        start_mbid: Option<String>,
-
-        /// Whether to check FILO (first in, last out) instead of FIFO (first in, first out)
-        #[arg(short, long)]
-        new_first: bool,
-
-        /// List of lints that should only be checked (Note: Put this argument last or before another argument)
-        #[arg(short, long, num_args = 0..)]
-        whitelist: Option<Vec<String>>,
-
-        /// List of lints that should not be checked (Note: Put this argument last or before another argument)
-        #[arg(short, long, num_args = 0..)]
-        blacklist: Option<Vec<String>>,
-    },
+    Clippy(MusicbrainzClippyCommand),
 }
 
 impl MusicbrainzSubcommands {
-    pub async fn run(&self, conn: &mut sqlx::SqliteConnection) {
+    pub async fn run(&self, _conn: &mut sqlx::SqliteConnection) {
         match self {
-            Self::Clippy {
-                start_mbid,
-                new_first,
-                whitelist,
-                blacklist,
-            } => {
-                let mbid = start_mbid
-                    .clone()
-                    .unwrap_or_else(|| "8f3471b5-7e6a-48da-86a9-c1c07a0f47ae".to_string());
-
-                let filter = if let Some(whitelist) = whitelist {
-                    WhitelistBlacklist::WhiteList(whitelist.clone())
-                } else if let Some(blacklist) = blacklist {
-                    WhitelistBlacklist::BlackList(blacklist.clone())
-                } else {
-                    WhitelistBlacklist::BlackList(Vec::new())
-                };
-
-                mb_clippy(
-                    conn,
-                    &read_mbid_from_input(&mbid).expect("Couldn't read mbid"),
-                    *new_first,
-                    &filter,
-                )
-                .await;
-            }
+            Self::Clippy(val) => val.run().await,
         }
     }
 }
