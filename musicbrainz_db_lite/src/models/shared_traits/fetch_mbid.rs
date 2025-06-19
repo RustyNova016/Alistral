0d57a38aa012681352ab7628b46d084069b7dc0e
RustyNova <rusty.nova.jsb@gmail.com>
2025-06-19 08:20:46 +0000

    fix: Use task to fetch data

--- musicbrainz_db_lite/src/models/shared_traits/fetch_mbid.rs
+++ musicbrainz_db_lite/src/models/shared_traits/fetch_mbid.rs
@@ -1,3 +1,5 @@
+use std::sync::Arc;
+
 use crate::DBClient;
 
 pub trait FetchMBID<T> {
@@ -5,4 +7,20 @@ pub trait FetchMBID<T> {
         client: &DBClient,
         mbid: &str,
     ) -> impl std::future::Future<Output = Result<T, musicbrainz_rs_nova::Error>> + Send;
+
+    fn fetch_from_mbid_as_task(
+        client: Arc<DBClient>,
+        mbid: &str,
+    ) -> impl std::future::Future<Output = Result<T, musicbrainz_rs_nova::Error>> + Send
+    where
+        T: Send + 'static,
+    {
+        async {
+            let mbid = mbid.to_owned();
+
+            tokio::spawn(async move { Self::fetch_from_mbid(client.as_ref(), &mbid).await })
+                .await
+                .unwrap()
+        }
+    }
 }
