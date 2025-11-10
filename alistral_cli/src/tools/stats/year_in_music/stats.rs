7db9e349684e03c8bf20ae493503b1ace04dbd17
RustyNova <rusty.nova.jsb@gmail.com>
2025-11-10 10:38:03 +0100

    feat: Added release ghroup stat compiling

--- alistral_cli/src/tools/stats/year_in_music/stats.rs
+++ alistral_cli/src/tools/stats/year_in_music/stats.rs
@@ -6,11 +6,14 @@ impl YimReport {
     pub async fn prefetch_data(&self) {
         self.full_user_stats.recording_stats().await.unwrap();
         self.full_user_stats.artists_stats().await.unwrap();
+        self.full_user_stats.release_group_stats().await.unwrap();
+
         self.current.recording_stats().await.unwrap();
         self.current.artists_stats().await.unwrap();
+        self.current.release_group_stats().await.unwrap();
     }
 
     pub async fn num_listens_in_year(&self) -> usize {
         self.current.listens().listen_count()
-    }   
+    }
 }
